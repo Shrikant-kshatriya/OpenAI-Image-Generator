3e1a466a7bfe14e802fcec8315ffa9688902f07c
Shrikant-kshatriya <shrikant070920@gmail.com>
2022-12-08 20:35:09 +0530

    Front-end complete

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.use(express.static('public'));
+
 app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
