77e71639b1204610a9f83cb85b074209def853e8
Shrikant-kshatriya <shrikant070920@gmail.com>
2022-12-08 19:12:41 +0530

    OpenAI backend complete

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
